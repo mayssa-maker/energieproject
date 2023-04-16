7150a1194c4196d749a86a782993f549a9603673
mayssa-maker <mattelimayssa@gmail.com>
2023-04-16 17:46:38 +0100

    addAdminUser

--- energie/admin.py
+++ energie/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
+
+
+
+
 
 # Register your models here.
+admin.site.register(Compte)
+admin.site.register(Societe)
+admin.site.register(HistoriqueCalcul)
+admin.site.register(TotalEnergie)
+admin.site.register(Dynef)
+admin.site.register(Compteur)
